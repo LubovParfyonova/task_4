e03e8800ff720ae7723e705693d46e88826f87a3
Lubov <lubaparfyonova@gmail.com>
2022-01-04 06:17:41 +0200

    Add script

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Конвертер
+
+const chooseOperation = prompt('Выберите опрацию: \n 1 - преобразовать доллар в грн; \n 2 - преобразовать грн в доллар');
+
+const num = +prompt('Введите сумму для преобразования');
+
+const res = converter(num,chooseOperation);
+console.log(res);
+
+
+function getGrn(x){
+    return x * 27.29;
+}
+function getDolar(x){
+    return x / 27.29;
+}
+
+function converter(number, operation){
+
+        let i;
+
+        switch (operation){
+            case '1':
+                i = getGrn;
+                break;
+            case '2':
+                i = getDolar;
+                break;
+        }
+
+        if (typeof i === 'function'){
+            const result = i (number);
+            return result;
+        }else{
+            alert('Неправильно выбрана операция');
+            return null;
+        }
+}
